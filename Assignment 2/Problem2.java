eef8f912f3f036f2269d1ca0395aff4acad79317
Sasank Tadepalli <saisasank25@gmail.com>
2022-09-11 11:38:46 -0700

    Problem 2 Almost Done

--- Assignment 2/Problem2.java
+++ Assignment 2/Problem2.java
@@ -31,6 +31,68 @@ class Tree{
 		return root;
 	}
 
+	void convertToHeap(){
+		heapify(this.root);
+	}
+
+	void heapify(Node root){
+		if(root == null) return;
+		
+		heapify(root.left);
+		heapify(root.right);
+
+		swapNodes(root);
+	}
+
+	void swapNodes(Node root){
+
+		if(root.left == null && root.right == null) return;
+		if(root.left == null){
+			if(root.value < root.right.value){
+				int value = root.value;
+				root.value = root.right.value;
+				root.right.value = value;
+				swapNodes(root.right);
+			}
+		}
+		else if(root.right == null){
+			if(root.value < root.left.value){
+				int value = root.value;
+				root.value = root.left.value;
+				root.left.value = value;
+				swapNodes(root.left);
+			}
+		}
+		else{
+			if(root.value < root.left.value && root.value > root.right.value){
+				int value = root.value;
+				root.value = root.left.value;
+				root.left.value = value;
+				swapNodes(root.left);
+			}
+			else if(root.value < root.right.value && root.value > root.left.value){
+				int value = root.value;
+				root.value = root.right.value;
+				root.right.value = value;
+				swapNodes(root.right);
+			}
+			else if(root.value < root.right.value && root.value < root.left.value){
+				if(root.left.value < root.right.value){
+					int value = root.value;
+					root.value = root.right.value;
+					root.right.value = value;
+					swapNodes(root.right);
+				}
+				else{
+					int value = root.value;
+					root.value = root.left.value;
+					root.left.value = value;
+					swapNodes(root.left);
+				}
+			}
+		}
+	}
+
 	void traverse(){
 		ArrayList<Node> queue = new ArrayList<Node>();
 		queue.add(this.root);
@@ -42,6 +104,8 @@ class Tree{
 			if(node.left != null) queue.add(node.left);
 			if(node.right != null) queue.add(node.right);
 		}
+
+		System.out.println();
 	}
 }
 
@@ -49,6 +113,8 @@ class Problem2{
 	public static void main(String args[]){
 		int[] values = new int[]{24, 12, 38, 3, 45, 2, 56, 8, 100, 6};
 		Tree tree = new Tree(values);
+		tree.traverse();
+		tree.convertToHeap();
 		tree.traverse();
 	}
 }
