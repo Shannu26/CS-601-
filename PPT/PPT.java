1dedca9aeb9cf38f63c67e8896032cd1ca0dc5e7
Sasank Tadepalli <saisasank25@gmail.com>
2022-10-15 12:51:12 -0700

    PPT Update

--- PPT/PPT.java
+++ PPT/PPT.java
@@ -1,5 +1,28 @@
 
 class PPT{
+
+	public static long naiveLogic(int x, int y){
+		int n = 0;
+		int xTemp = x;
+		int yTemp = y;
+
+		while(xTemp != 0){
+			n += 1;
+			xTemp = xTemp / 10;
+		}
+
+		int shift = 1;
+		while(x != 0){
+			int xTemp = x % 10;
+			long product = 1;
+			long multiplier = 1;
+			yTemp = y;
+			while(yTemp != 0){
+				int temp = 
+			}
+		}
+	}
+
 	public static void main(String args[]){
 		
 	}
